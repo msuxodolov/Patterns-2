936f6870d627aed9c3a6516e095203c5f01dffe5
Suhodolov Matwey <Msuxodolov18@gmail.com>
2024-03-22 23:14:40 +0300

    change build.gradle

--- src/test/java/ru/netology/testmode/data/DataGenerator.java
+++ src/test/java/ru/netology/testmode/data/DataGenerator.java
@@ -7,7 +7,6 @@ import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 import lombok.Value;
-import lombok.val;
 
 import java.util.Locale;
 
